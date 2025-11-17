e6277358058a8ad687c8f6543b2b11ea06f100cf
Mick <243338@student.scalda.nl>
2025-11-17 13:44:38 +0100

    Update reCAPTCHA script source to use environment variable for site key

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -26,7 +26,7 @@ export default function Contact() {
 
   useEffect(() => {
     const script = document.createElement('script');
-    script.src = `https://www.google.com/recaptcha/api.js?render=explicit`;
+    script.src = `https://www.google.com/recaptcha/api.js?render=${process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY}`;
     script.async = true;
     script.defer = true;
     
